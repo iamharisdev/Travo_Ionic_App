e83a58eb47070824b9995c9c73c518a8a0748ff8
Carlos Buenrostro <c.buenrostro.dev@gmail.com>
2024-08-22 23:47:53 -0600

    feat(PM-1011): create sign in animation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -9,7 +9,7 @@ import {
   IonPage,
   IonText,
 } from "@ionic/react";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import TrovaLogo from "../../../public/assets/TrovaLogo.png";
 import { FORGOT_PASSWORD, LANDING } from "../../shared/routes/routes";
 import { useHistory } from "react-router";
@@ -21,6 +21,7 @@ const CSSprefix = 'login';
 const Login: React.FC = (): React.ReactElement => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [showingAnimation, setShowingAnimation] = useState<undefined | boolean>();
   const history = useHistory();
 
   const disableButton = useMemo(
@@ -28,66 +29,86 @@ const Login: React.FC = (): React.ReactElement => {
     [email, password]
   );
 
+  useEffect(() => {
+    if (showingAnimation === undefined) {
+      setShowingAnimation(true);
+    }
+    if (showingAnimation) {
+      setTimeout(() => setShowingAnimation(false), 1800);
+    }
+  }, [showingAnimation]);
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div className={`${CSSprefix} ion-padding`}>
-          <IonItem lines="none">
+        <div className={`${showingAnimation === true ? 'transition-container' : `${CSSprefix}-main-container`} ion-padding`}>
+          <IonItem
+            lines="none"
+            slot="start"
+            id={showingAnimation ? 'float' : ''}
+            className={showingAnimation ? 'transition-logo-item' : `${CSSprefix}-logo-item`}
+          >
             <IonImg
               className={`${CSSprefix}-logo`}
               src={TrovaLogo}
               alt="Trova Logo"
             />
           </IonItem>
-          <IonItem lines="none">
-            <IonText className={`${CSSprefix}-sign-in`}>
-              Sign in
-            </IonText>
-          </IonItem>
-          <IonItem lines="none">
-            <IonText
-              color="dark"
-              className={`${CSSprefix}-welcome`}
-            >
-              Welcome back! Please enter your details.
-            </IonText>
-          </IonItem>
-          <IonItem lines="none" className="custom-input ion-margin-bottom">
-            <IonLabel position="stacked" class="custom-input">Email address</IonLabel>
-            <IonInput
-              class="custom"
-              type="email"
-              placeholder="Enter email address"
-              onIonInput={(e) => setEmail(e.detail.value || "")}
-            />
-          </IonItem>
-          <IonItem lines="none" className="custom-input ion-margin-bottom">
-            <IonLabel position="stacked" class="custom-input">Password</IonLabel>
-            <IonInput
-              class="custom"
-              type="password"
-              placeholder="Enter your password"
-              onIonInput={(e) => setPassword(e.detail.value || '')}
-            >
-              <IonInputPasswordToggle slot="end" color="dark" />
-            </IonInput>
-          </IonItem>
-          <IonButton
-            href={FORGOT_PASSWORD}
-            className={`${CSSprefix}-forgot-password`}
-            fill="clear"
-          >
-            Forgot Password?
-          </IonButton>
-          <IonButton
-            className="login-button"
-            color="primary"
-            disabled={disableButton}
-            expand="block"
-            onClick={() => history.push(LANDING)}
-          >
-            Sign in
-          </IonButton>
+          {showingAnimation === false && (
+            <>
+              <IonItem lines="none" className="ion-no-padding">
+                <IonText className={`${CSSprefix}-sign-in`}>
+                  Sign in
+                </IonText>
+              </IonItem>
+              <IonItem lines="none" className="ion-no-padding ion-margin-bottom">
+                <IonText
+                  color="dark"
+                  className={`${CSSprefix}-welcome`}
+                >
+                  Welcome back! Please enter your details.
+                </IonText>
+              </IonItem>
+              <IonItem lines="none" className="custom-input ion-margin-bottom">
+                <IonLabel position="stacked" class="custom-input">Email address</IonLabel>
+                <IonInput
+                  class="custom"
+                  type="email"
+                  placeholder="Enter email address"
+                  value={email}
+                  onIonInput={(e) => setEmail(e.detail.value || "")}
+                />
+              </IonItem>
+              <IonItem lines="none" className="custom-input ion-margin-bottom">
+                <IonLabel position="stacked" class="custom-input">Password</IonLabel>
+                <IonInput
+                  class="custom"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onIonInput={(e) => setPassword(e.detail.value || '')}
+                >
+                  <IonInputPasswordToggle slot="end" color="dark" />
+                </IonInput>
+              </IonItem>
+              <IonButton
+                href={FORGOT_PASSWORD}
+                className={`${CSSprefix}-forgot-password`}
+                fill="clear"
+              >
+                Forgot Password?
+              </IonButton>
+              <IonButton
+                className="login-button"
+                color="primary"
+                disabled={disableButton}
+                expand="block"
+                onClick={() => history.push(LANDING)}
+              >
+                Sign in
+              </IonButton>
+            </>
+          )}
         </div>
       </IonContent>
     </IonPage>
