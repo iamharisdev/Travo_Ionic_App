f2d77f601c4cb9e453a0013d918de8f5cb1b4d41
Carlos Buenrostro <c.buenrostro.dev@gmail.com>
2024-09-02 21:29:49 -0600

    feat(PM-1013): add custom spinner class

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
     if (loading) {
-      present({ message, spinner: "bubbles", cssClass: "spinner" });
+      present({ message, spinner: "bubbles", cssClass: "spinner custom-loading" });
     }
 
     if (!loading) {
