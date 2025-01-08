e19f558d2a963f406003af94803e84afda6d0e4c
Carlos Buenrostro <c.buenrostro.dev@gmail.com>
2025-01-07 20:39:48 -0600

    bugfix(PM-1283): fix dates always the same in calendar month and get events from 6 months in the past

--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -70,7 +70,7 @@ const Loading: React.FC = (): React.ReactElement => {
             await dispatch(getEventsAction({
               practiceId: providerPractice.practiceId,
               providerId: providerPractice.providerId,
-              start: dayjs().startOf('year').toISOString(),
+              start: dayjs().subtract(6, 'months').toISOString(),
               end: dayjs().add(1, 'year').endOf('year').toISOString(),
               pageNumber: 0,
               pageSize: 999,
